35a2ea8044f38df0062757f29b7c792d3b3589d9
門田朋己 <monndenn15@gmail.com>
2019-03-25 00:03:24 +0900

    ListViewの内容をString-arrayから取得するよう変更

--- app/src/main/java/com/example/tmk815/digitaldenpyo/NewOrderFragment.kt
+++ app/src/main/java/com/example/tmk815/digitaldenpyo/NewOrderFragment.kt
@@ -8,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SimpleAdapter
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.neworder.*
 
 
@@ -23,7 +25,9 @@ class NewOrderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         ViewCompat.setNestedScrollingEnabled(this.menu_list, true)
-        val items = List(20) { mapOf("name" to "title-$it", "price" to "$it") }
+        val menuName = resources.getStringArray(R.array.menu_name)
+        val menuPrice = resources.getStringArray(R.array.menu_price);
+        val items = List(menuName.size) { mapOf("name" to menuName[it], "price" to menuPrice[it]+"円") }
         val adapter = SimpleAdapter(
             this.context,
             items,
@@ -32,6 +36,11 @@ class NewOrderFragment : Fragment() {
             intArrayOf(android.R.id.text1, android.R.id.text2)
         )
         menu_list.adapter = adapter
+
+        menu_list.setOnItemClickListener { adapterView, view, position, id ->
+            val textView = view.findViewById<TextView>(android.R.id.text1)
+            Toast.makeText(this.context, "Clicked: ${textView.text}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onAttach(context: Context?) {
