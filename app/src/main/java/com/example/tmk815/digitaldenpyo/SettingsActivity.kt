d06a515d83ecb6a9ba9280d513f383b7d77297ee
門田朋己 <monndenn15@gmail.com>
2019-03-29 12:40:44 +0900

    座席を追加する機能を追加したことによる設定画面の修正

--- app/src/main/java/com/example/tmk815/digitaldenpyo/SettingsActivity.kt
+++ app/src/main/java/com/example/tmk815/digitaldenpyo/SettingsActivity.kt
@@ -1,12 +1,29 @@
 package com.example.tmk815.digitaldenpyo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeButtonEnabled(true)
+        } ?: IllegalAccessException("Toolbar cannot be null")
+
+        list_setting.setOnItemClickListener { parent, view, position, id ->
+            if (position == 0) {
+                val settingIntent = Intent(this, NewMenu::class.java)
+                startActivity(settingIntent)
+            } else if (position == 1) {
+                val settingIntent = Intent(this, Seat::class.java)
+                startActivity(settingIntent)
+            }
+        }
     }
 }
