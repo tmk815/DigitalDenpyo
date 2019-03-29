126fa91fbb9be728cdc61517c6b4327ad0630635
門田朋己 <monndenn15@gmail.com>
2019-03-29 17:04:08 +0900

    ログインされていれば座席を追加

--- app/src/main/java/com/example/tmk815/digitaldenpyo/Seat.kt
+++ app/src/main/java/com/example/tmk815/digitaldenpyo/Seat.kt
@@ -2,6 +2,7 @@ package com.example.tmk815.digitaldenpyo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_seat.*
 
@@ -20,8 +21,10 @@ class Seat : AppCompatActivity() {
 
         submitSeat.setOnClickListener {
             submitSeat.setOnClickListener {
+                val user = FirebaseAuth.getInstance().currentUser
+
                 val database = FirebaseDatabase.getInstance()
-                val myRef = database.getReference("seat").child(editSeat.text.toString())
+                val myRef = database.getReference("seat").child(user!!.uid).child(editSeat.text.toString())
 
                 myRef.setValue((editCount.text.toString()).toInt())
             }
