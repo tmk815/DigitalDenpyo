2d301e7b6d2de1704977dcb0e120fb869a74880b
門田朋己 <monndenn15@gmail.com>
2019-03-29 21:40:44 +0900

    ログイン後画面遷移

--- app/src/main/java/com/example/tmk815/digitaldenpyo/FirebaseUIActivity.kt
+++ app/src/main/java/com/example/tmk815/digitaldenpyo/FirebaseUIActivity.kt
@@ -55,12 +55,15 @@ class FirebaseUIActivity : AppCompatActivity() {
                 // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser
                 Log.d("UID", user?.uid)
+                val settingIntent = Intent(this, MainActivity::class.java)
+                startActivity(settingIntent)
+                finish()
                 // ...
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
                 // response.getError().getErrorCode() and handle the error.
-                // ...
+                Log.d("LoginError", response?.error?.errorCode.toString())
             }
         }
     }
