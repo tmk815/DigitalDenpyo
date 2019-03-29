82d7731e9a61a6399b2f491fa413b1ac1ce5c38a
門田朋己 <monndenn15@gmail.com>
2019-03-29 17:03:07 +0900

    認証されていれば新規メニューをuid以下に書き込み

--- app/src/main/java/com/example/tmk815/digitaldenpyo/NewMenu.kt
+++ app/src/main/java/com/example/tmk815/digitaldenpyo/NewMenu.kt
@@ -2,6 +2,7 @@ package com.example.tmk815.digitaldenpyo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.menu_new.*
 
@@ -16,8 +17,24 @@ class NewMenu : AppCompatActivity() {
         } ?: IllegalAccessException("Toolbar cannot be null")
 
         submitMenu.setOnClickListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            /*user?.let {
+                // Name, email address, and profile photo Url
+                val name = user.displayName
+                val email = user.email
+                val photoUrl = user.photoUrl
+
+                // Check if user's email is verified
+                val emailVerified = user.isEmailVerified
+
+                // The user's ID, unique to the Firebase project. Do NOT use this value to
+                // authenticate with your backend server, if you have one. Use
+                // FirebaseUser.getToken() instead.
+                val uid = user.uid
+            }*/
+
             val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("menu").child(editName.text.toString())
+            val myRef = database.getReference("menu").child(user!!.uid).child(editName.text.toString())
 
             myRef.setValue((editPrice.text.toString()).toInt())
         }
