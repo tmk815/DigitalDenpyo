e135aad4ad7beb2939cda08cfe4bbaf506b5cdc2
門田朋己 <monndenn15@gmail.com>
2019-04-01 15:55:58 +0900

    コンストラクタに移動

--- app/src/main/java/com/example/tmk815/digitaldenpyo/TabsPagerAdapter.kt
+++ app/src/main/java/com/example/tmk815/digitaldenpyo/TabsPagerAdapter.kt
@@ -1,14 +1,9 @@
 package com.example.tmk815.digitaldenpyo
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-
 class TabsPagerAdapter(
     fm: androidx.fragment.app.FragmentManager,
-    tabsFragments: ArrayList<Class<out androidx.fragment.app.Fragment>>
+    private val tabsFragments: ArrayList<Class<out androidx.fragment.app.Fragment>>
 ) : androidx.fragment.app.FragmentPagerAdapter(fm) {
-    val tabsFragments: ArrayList<Class<out androidx.fragment.app.Fragment>> = tabsFragments
 
     override fun getItem(position: Int): androidx.fragment.app.Fragment {
         return tabsFragments[position].newInstance()
